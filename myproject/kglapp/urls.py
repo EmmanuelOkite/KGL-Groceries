339e6cab5bb2da46abd03b9a98bebd52833f8f76
EmmanuelOkite <emmanuelokite3@gmail.com>
2025-03-15 11:13:15 +0300

    i added a new model

--- myproject/kglapp/urls.py
+++ myproject/kglapp/urls.py
@@ -11,5 +11,12 @@ from django.urls import path
 from .views import landing_page
 
 urlpatterns = [
-    path('', landing_page, name='landing_page'),  # This sets the landing page as the homepage
+    path('', landing_page, name='landing_page'), 
+    path('branches',branches, name="branches"),
+    path('base', base, name="base"),
+    path('director', director, name="director"),
+    path('manager', manager, name="manager"),
+    path('attendant', attendant, name="attendant"),
+    path('create/', views.create_produce, name='create_produce'),
+    path('list/', views.produce_list, name='produce_list'),
 ]
--- myproject/kglapp/views.py
+++ myproject/kglapp/views.py
@@ -3,6 +3,10 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -13,3 +17,33 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing.html')  # Ensure this template exists
+
+def branches(request):
+    return render(request, 'branches.html') 
+
+def base(request):
+    return render(request, 'base.html')
+
+def director(request):
+    return render(request, 'director.html')
+
+def manager(request):
+    return render(request, 'manager.html')
+
+def attendant(request):
+    return render(request, 'attendant.html')
+
+def create_produce(request):
+    if request.method == 'POST':
+        form = ProduceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('produce_list')
+    else:
+        form = ProduceForm()
+    produces = Produce.objects.all()
+    return render(request, 'add_produce.html', {'form': form, 'produces': produces})
+
+def produce_list(request):
+    produces = Produce.objects.all()
+    return render(request, 'produce_list.html', {'produces': produces})
